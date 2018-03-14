98567c60c1f2dd54edd5c6e19166615ffe131a42
Giorgos Kavallieratos <egiokav@gmail.com>
2018-03-14 18:59:49 +0200

    setup separate test and development envs corr3

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
+console.log('======env=====',process.env.NODE_ENV);
 var env = process.env.NODE_ENV || 'development'; // if production , this will be set to 'production'
-
+console.log('*******env******', env);
 if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // use the built-in  promise library as opposed to some 3rd party one
 
 // create this block for heroku. if localhost db there select or go to mlab.com
-let db = process.env.MONGODB_URI;
+var db = process.env.MONGODB_URI;
 
 mongoose.connect(db);
 
