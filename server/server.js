bcf0451bc7734af2447f77020b728c8f6af8d41a
Giorgos Kavallieratos <egiokav@gmail.com>
2018-03-14 17:05:52 +0200

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const {ObjectID} = require('mongodb');
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -68,10 +69,33 @@ app.delete('/todos/:id', (req,res) => {
 
 });
 
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);  // allows specific properties to be picked and passed ot target obhect while protecting others
+    if (!ObjectID.isValid(id)) {
+         console.log('ID not valid', id);
+         return res.status(404).send();
+    }
 
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then ((todo) => {//set body in object and prefer the new one than the original  
+        if (!todo ) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch ((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`started server at port ${port}...`);
 });
 
 module.exports = {app};
+ 
