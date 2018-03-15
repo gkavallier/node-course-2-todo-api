00d0bb7930ea607a68e3fcf148a54e55579fa048
Giorgos Kavallieratos <egiokav@gmail.com>
2018-03-15 20:08:09 +0200

    Add POST /users route and tokern method

---
+++ playground/hashing.js
@@ -0,0 +1,41 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+// commented out in order to use jsonwebtoken instead. below records use crypto-js
+//
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message : ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash) {
+//     console.log('data wasnt changed.');
+// } else  {
+//     console.log('data was changed, dont trust!')
+// }
+
+// below equivalent with jsonwebtoken
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,68 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const jwt = require('jsonwebtoken');
+
+const _ = require('lodash');
 
 
 //create mongoose model so mongoose knows how to store data
 
-var User = mongoose.model('User', {
-    email: {
+var UserSchema = new mongoose.Schema({
+        email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator:  validator.isEmail ,  
+            message: '{VALUE} is not a valid email'
+        }
+    }, 
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 });
+
+
+// arrow functions don't bind this keyword thats why we use classical function
+//
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject(); // creates object taking mongoose user var with only the available properties existing
+    
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token =  jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+    // user.tokens.push({access,token});
+    // push method above also works for me but for later versions of mongo concat method below works only
+    user.tokens = user.tokens.concat([{
+        access: access, 
+        token: token
+    }]);
+    return user.save().then(() => {
+        return token; // returning value not function in promise because this will be used by anoter then in server
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
